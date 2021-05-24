a63de9f59e83a5799f223e92fa1e00b6ef011bfc
krupanand <krupa8522@gmail.com>
2021-05-24 15:37:29 +0530

    added 2 lists comparision

--- practice1.py
+++ practice1.py
@@ -1,1 +1,11 @@
-print("hello")
+
+# Program to Compare two Lists
+
+
+lst1=[10,20,30,'krupa', 'amazon', 'iisc','zerodha']
+lst2=[40,50,20,60,'iit', 'iisc', 'google', 'spacex', 'zerodha']
+
+for i in lst1:
+    for j in lst2:
+        if i==j:
+            print("the Common element is " , i)
